cd4dd8a5026179da09e866ed74742b3cb0ad08fe
gcousin <gcousin@greensystemes.com>
2021-03-26 16:19:22 +0100

    all file

---
+++ app.js
@@ -0,0 +1,30 @@
+const puppeteer = require('puppeteer');
+const fs = require("fs");
+
+const dateFolder = new Date().toISOString().slice(0, 7);
+const dateFile = new Date().getDate();
+console.log(dateFolder);
+console.log(dateFile);
+
+if (!fs.existsSync('screen')) {
+    fs.mkdirSync('screen');
+}
+
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://dette.gcousin.site');
+
+    await page.setViewport({
+        width: 1500,
+        height: 800
+    });
+
+    if (!fs.existsSync('screen/' + dateFolder)) {
+        fs.mkdirSync('screen/' + dateFolder);
+    }
+
+    await page.screenshot({ path: 'screen/' + dateFolder + '/' + dateFile + '.png' });
+    await browser.close();
+})();
